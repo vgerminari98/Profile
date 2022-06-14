e9cef5b3d660a26f2aac0b4783b45df7aa242094
vgerminari98 <vgerminari98@outlook.com>
2022-06-14 20:16:32 -0300

    commit inicial

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import './style.css'
+
+export function Header() {
+
+   return (
+     <div className='container'>
+      <strong>Ilustranaty Design Digital</strong>
+     
+        <div className="list">
+            <nav>
+               <ul>
+                  <li>Home</li>
+                  <li>Habilidades</li>
+                  <li>Projetos</li> 
+               </ul>
+            </nav>
+        </div>
+      
+     </div>
+   )
+
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import { Header } from '../../components/Header'
+
+export function Home() {
+
+  return (
+   <div className='App'>
+      <div className='header'>
+        <Header />
+      </div>
+   </div>
+  )
+}
